4693d6962c006ba445c31de1065451aeed9a932a
stigler20 <yannstiglerboussoukaboussouka@gmail.com>
2023-06-12 19:14:51 +0200

    Mon devoir

---
+++ Assets/JS/app.js
@@ -0,0 +1,7 @@
+function supprimerElement(element) {
+    element.parentNode.removeChild(element);
+}
+/*Exercice 14 :
+Créez une liste à puces avec plusieurs éléments de liste. Lorsque vous cliquez sur un élément de liste, utilisez JavaScript pour le supprimer de la liste.
+*/
+//pour cette exercice nous avons utiliser uniquement la fonction supprimerElement
